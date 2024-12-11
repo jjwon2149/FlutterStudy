9ba8d4a55f29e9c04df1e9d08e27b0d664ba4517
jjwon2149 <jjwon2149@gmail.com>
2024-12-11 22:52:27 +0900

    feat: add Dice - 터치시 랜덤 Dice - StatefulWidget로 변경

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,10 +16,83 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  int randomDiceNumber() {
+    return Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      //옵션 + 엔터로 센터 감싸기.
+      child: Row(
+        children: [
+          Expanded(
+              child: TextButton(
+            style: TextButton.styleFrom(backgroundColor: Colors.red),
+            onPressed: () {
+              setState(() {
+                leftDiceNumber = randomDiceNumber();
+                rightDiceNumber = randomDiceNumber();
+                print(leftDiceNumber);
+              });
+            },
+            child: Image.asset('images/dice$leftDiceNumber.png'),
+          )),
+          Expanded(
+            child: TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.red),
+                onPressed: () {
+                  setState(() {
+                    leftDiceNumber = randomDiceNumber();
+                    rightDiceNumber = randomDiceNumber();
+                    print(rightDiceNumber);
+                  });
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png')),
+          ),
+        ],
+      ),
+    );
   }
 }
 
+// class DicePage extends StatelessWidget {
+//   int leftDiceNumber = 1;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     leftDiceNumber = 3;
+//
+//     return Center(
+//       //옵션 + 엔터로 센터 감싸기.
+//       child: Row(
+//         children: [
+//           Expanded(
+//               child: TextButton(
+//                 style: TextButton.styleFrom(backgroundColor: Colors.red),
+//                 onPressed: () {},
+//                 child: Image.asset('images/dice$leftDiceNumber.png'),
+//               )),
+//           Expanded(
+//             child: TextButton(
+//                 style: TextButton.styleFrom(backgroundColor: Colors.red),
+//                 onPressed: () {},
+//                 child: Image.asset('images/dice4.png')),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
